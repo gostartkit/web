9a0f15f3d062d50525908ce0b8a3498a19546d54
sskycn <sskycn@gmail.com>
2018-09-11 23:00:38 +0800

    added some base functions.

--- application.go
+++ application.go
@@ -1,13 +1,17 @@
 package web
 
-import "sync"
+import (
+	"net/http"
+	"sync"
+)
 
 var app *Application
 var once sync.Once
 
 // Application is type of a web.Application
 type Application struct {
-	ctx *Context
+	Context *Context
+	Keys    []string
 }
 
 // CreateApplication return a singleton web.Application
@@ -18,13 +22,33 @@ func CreateApplication() *Application {
 	return app
 }
 
+// Use add the given middleware function to web.Application.
+func (app *Application) Use(fn func(ctx *Context)) *Application {
+	return app
+}
+
+// On add event
+func (app *Application) On(name string, fn func(err string, ctx *Context)) *Application {
+	return app
+}
+
+// Listen addr
+func (app *Application) Listen(addr string) *Application {
+	http.ListenAndServe(addr, nil)
+	return app
+}
+
+// Inspect method
+func (app *Application) Inspect() string {
+	return ""
+}
+
 // createApplication return a web.Application
 func createApplication() *Application {
 	return &Application{}
 }
 
-// Use add the given middleware function to web.Application.
-func (app *Application) Use(fn func(ctx *Context)) *Application {
-	fn(app.ctx)
-	return app
+// createContext return a web.Context
+func createContext() *Context {
+	return &Context{}
 }
--- context.go
+++ context.go
@@ -2,4 +2,11 @@ package web
 
 // Context is type of an web Context
 type Context struct {
+	Application *Application
+	Response    *Response
+	Request     *Request
+}
+
+func (ctx *Context) Cookies() {
+
 }

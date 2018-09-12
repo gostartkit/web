c3537761490329cefaa2c4acc1e42b8b7b763b5f
sskycn <sskycn@gmail.com>
2018-09-12 14:20:26 +0800

    added config.go and application_test.go files.

---
+++ config.go
@@ -0,0 +1,13 @@
+package web
+
+// Config is type of a web.Config
+type Config struct {
+}
+
+func (cfg *Config) Get(name string) string {
+	return ""
+}
+
+func (cfg *Config) Set(name string, value string) {
+
+}
--- request.go
+++ request.go
@@ -1,5 +1,5 @@
 package web
 
-// Request is type of an web Request
+// Request is type of a web.Request
 type Request struct {
 }

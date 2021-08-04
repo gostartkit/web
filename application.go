0625f726317e34602dc159bf796442a2958df70f
sskycn <sskycn@gmail.com>
2021-08-04 11:33:22 +0800

    clean

--- application.go
+++ application.go
@@ -127,6 +127,13 @@ func (app *Application) addRoute(method, path string, cb Callback) {
 	if pc := countParams(path); pc > app.maxParams {
 		app.maxParams = pc
 	}
+
+	if app.paramsPool.New == nil && app.maxParams > 0 {
+		app.paramsPool.New = func() interface{} {
+			ps := make(Params, 0, app.maxParams)
+			return &ps
+		}
+	}
 }
 
 // ServeFiles ("/src/*filepath", http.Dir("/var/www"))
@@ -235,13 +242,6 @@ func (app *Application) serve(addr string, listener net.Listener, fns ...func(*h
 
 	for _, fn := range fns {
 		fn(srv)
-	}
-
-	if app.paramsPool.New == nil && app.maxParams > 0 {
-		app.paramsPool.New = func() interface{} {
-			ps := make(Params, 0, app.maxParams)
-			return &ps
-		}
 	}
 
 	if err := srv.Serve(listener); err != nil {

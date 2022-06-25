ed9d5ce04152d1336d4cd09ad2622b9f3f9fb61c
sskycn <sskycn@gmail.com>
2022-06-25 17:26:51 +0800

    clean

--- type.go
+++ type.go
@@ -1,5 +1,14 @@
 package web
 
+const (
+	QueryType     = "$type"
+	QueryFilter   = "$filter"
+	QueryOrderBy  = "$orderBy"
+	QueryPage     = "$page"
+	QueryPageSize = "$pageSize"
+	HeaderAttrs   = "$attrs"
+)
+
 // Reader function
 type Reader func(ctx *Context, v Data) error
 

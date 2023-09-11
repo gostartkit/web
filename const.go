56cd4edd87d113bd787bbc801256b0978b208d8b
sskycn <sskycn@gmail.com>
2023-09-11 17:44:15 +0800

    removed sql filter and sql order by

---
+++ const.go
@@ -0,0 +1,10 @@
+package web
+
+const (
+	QueryType     = "_type"
+	QueryFilter   = "_filter"
+	QueryOrderBy  = "_orderBy"
+	QueryPage     = "_page"
+	QueryPageSize = "_pageSize"
+	HeaderAttrs   = "_attrs"
+)
--- type.go
+++ type.go
@@ -1,14 +1,5 @@
 package web
 
-const (
-	QueryType     = "_type"
-	QueryFilter   = "_filter"
-	QueryOrderBy  = "_orderBy"
-	QueryPage     = "_page"
-	QueryPageSize = "_pageSize"
-	HeaderAttrs   = "_attrs"
-)
-
 // Reader function
 type Reader func(ctx *Context, v Data) error
 

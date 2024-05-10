783eadae4121fdde027afcb428eb854c4a5ae536
sskycn <sskycn@gmail.com>
2024-05-10 23:21:09 +0800

    clean

--- type.go
+++ type.go
@@ -38,10 +38,10 @@ type Param struct {
 type Params []Param
 
 // Val get value from Params by name
-func (o Params) Val(name string) string {
-	for i := range o {
-		if o[i].Key == name {
-			return o[i].Value
+func (o *Params) Val(name string) string {
+	for i := range *o {
+		if (*o)[i].Key == name {
+			return (*o)[i].Value
 		}
 	}
 	return ""

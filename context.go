4b4ecba7789ff7990eba32fea7375f132eed1024
sskycn <sskycn@gmail.com>
2024-06-02 00:11:38 +0800

    clean

--- context.go
+++ context.go
@@ -651,10 +651,10 @@ func (c *Ctx) writeGOB(val any) error {
 
 // writeBinary Write Binary
 func (c *Ctx) writeBinary(val any) error {
-	return ErrContentType
+	return ErrNotImplemented
 }
 
 // writeAvro Write Avro
 func (c *Ctx) writeAvro(val any) error {
-	return ErrContentType
+	return ErrNotImplemented
 }
